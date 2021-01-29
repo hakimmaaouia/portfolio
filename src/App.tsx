fcd00bdf03b7d74cbf392b0794913aa6d066b5fa
hakimmaaouia <hakimmaaouia@gmail.com>
2021-01-29 21:59:43 +0100

    font

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  test
+  <h1>test</h1>
+  <p>Enumerating objects: 26, done.
+Counting objects: 100% (26/26), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (24/24), done.
+Writing objects: 100% (26/26), 214.13 KiB | 7.93 MiB/s, done.
+Total 26 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/hakimmaaouia/portfolio.git
+ * [new branch]      main main
+Branch 'main' set up to track remote branch 'main' from 'origin'.</p>
     </div>
   );
 }
