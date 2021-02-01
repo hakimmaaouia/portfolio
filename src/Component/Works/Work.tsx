4ba390da579433b2c1f53eb8568cd4e30c159f38
hakimmaaouia <hakimmaaouia@gmail.com>
2021-02-01 21:42:23 +0100

    add card work

--- src/Component/Works/Work.tsx
+++ src/Component/Works/Work.tsx
@@ -1,11 +1,15 @@
 import React from "react"
+import WorkCard from "./WorkCard"
 import "./Work.css"
 const Work=()=>{
     return(
 <div className="workContainer">
     <h2>My Recent Work</h2>
     <div className="project-grid">
-
+<WorkCard/>
+<WorkCard/>
+<WorkCard/>
+<WorkCard/>
         </div>
     </div>
     )
--- src/Component/Works/WorkCard.tsx
+++ src/Component/Works/WorkCard.tsx
@@ -1,2 +1,20 @@
 import React from "react"
-import "./Work.css"
+import "./WorkCard.css"
+
+const WorkCard=()=>{
+
+    return(
+        <div className="WorkCard">
+         <div className="overlay">
+             test
+         <div className="wordDesc">
+                 test2
+                 </div>
+         
+             </div> 
+
+        </div>
+    )
+}
+
+export default WorkCard;
