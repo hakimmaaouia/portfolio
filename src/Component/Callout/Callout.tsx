3167bb6e43b391d8bdd77cbf7cfebb4cb10a4156
hakimmaaouia <hakimmaaouia@gmail.com>
2021-02-02 21:32:22 +0100

    add callout

--- src/Component/Callout/Callout.tsx
+++ src/Component/Callout/Callout.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./Callout.css";
-import Bottom from "../Bottom/Bottom"
+import Bottom from "../Bottom/Bottom";
 
-const Callout=()=>{
-return(
-<div className="calloutContainer">
-    <h3>Interested in collaborating or investing?  </h3>
-    <p>I’m always open to discussing product design work or partnership opportunities.
-</p>
-    <Bottom>Start a conversation</Bottom>
-</div>
-)
-
-}
+const Callout = () => {
+  return (
+    <div className="calloutContainer">
+      <h3>Interested in collaborating or investing? </h3>
+      <p>
+        I’m always open to discussing product design work or partnership
+        opportunities.
+      </p>
+      <Bottom>Start a conversation</Bottom>
+    </div>
+  );
+};
 
 export default Callout;
