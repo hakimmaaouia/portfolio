2dad5d964e1b36d04b9821601dfac7fd3df91505
hakimmaaouia <hakimmaaouia@gmail.com>
2021-02-06 17:47:04 +0100

    ui contact

--- src/Component/Contact/Contact.tsx
+++ src/Component/Contact/Contact.tsx
@@ -1,23 +1,51 @@
-import React,{useState} from "react"
-import "./Contact.css"
-const Contact=()=>{
-const [Name, setName] = useState("");
-const [Email, setEmail] = useState("");
-const [Message,setMessage] = useState("");
+import React, { useState } from "react";
+import Bottom from "../Bottom/Bottom"
+import "./Contact.css";
+const Contact = () => {
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Message, setMessage] = useState("");
 
+  const sendEmail = () => {
+    fetch(
+      `http://127.0.0.1:4000/send-email?sender=${Email}&name=${Name}&message=${Message}`
+    ) //query string url
+      .catch((err) => console.error(err));
+  };
 
-const sendEmail=()=> {
-    fetch(`http://127.0.0.1:4000/send-email?sender=${Email}&name=${Name}&message=${Message}`) //query string url
-      .catch(err => console.error(err))
-  }
-
-    return(
-<div>
-<input type="text" name="Name"  onChange={(evet)=>setName(evet.target.value)}></input>
-<input type="text" name="Email"  onChange={(evet)=>setEmail(evet.target.value)}></input>
-<input type="text" name="Message" onChange={(evet)=>setMessage(evet.target.value)}></input>
-<button onClick={()=>sendEmail()}> Send Email </button>
+  return (
+    <div className="contactContainer">
+      <h1 className="titleContainer">
+        Thanks for taking the time to reach out. How can I help you today?
+      </h1>
+      <div className="form">
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="Name"
+            onChange={(evet) => setName(evet.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            name="Email"
+            onChange={(evet) => setEmail(evet.target.value)}
+          ></input>
+        </div>
+        <div className="inputtest">
+          <label>Message:</label>
+          <textarea
+            name="Message"
+            onChange={(evet) => setMessage(evet.target.value)}
+          ></textarea>
+        </div>
+      </div>
+      <Bottom>Send Email</Bottom>
+      <button onClick={() => sendEmail()}> Send Email </button>
     </div>
-    )
-}
-export default Contact
+  );
+};
+export default Contact;
