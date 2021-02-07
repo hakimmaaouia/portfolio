b9ff7aa5e1fb01a0311fcc9f9f1dba1f810093b7
hakimmaaouia <hakimmaaouia@gmail.com>
2021-02-07 13:15:13 +0100

    fix ui

--- src/Component/skills/Skills.tsx
+++ src/Component/skills/Skills.tsx
@@ -1,14 +1,16 @@
-import React from "react"
-import Card from "./Card"
-import "./Skills.css"
+import React from "react";
+import Card from "./Card";
+import "./Skills.css";
 
-const Skills=()=>{
-return(
-    <div className="SkillsContainer">
-<Card/>
-<Card/>   
-<Card/> 
-        </div>
-)
-}
+const Skills = () => {
+  return (
+    <div className="centreSkills">
+      <div className="SkillsContainer">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
 export default Skills;
--- src/Container/Main/Main.tsx
+++ src/Container/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import Herobody from "../../Component/Hero-body/Herobody";
 import Description from "../../Component/Description/Description";
 import Skills from "../../Component/skills/Skills";
@@ -6,14 +6,22 @@ import Work from "../../Component/Works/Work";
 import Callout from "../../Component/Callout/Callout";
 import Certif from "../../Component/Certif/Certif";
 
-const Main=()=>{
-  return( <>
-    <Herobody/>
-    <Description/>
-    <Skills/>
-    <Work/>
-    <Callout/>
-    <Certif/>
-    </>)
-}
-export default Main
+const Main = () => {
+  return (
+    <>
+      <Herobody />
+
+      <div style={{zIndex:-50}}>
+        <Description />
+      </div>
+      <div  style={{zIndex:50,marginTop:"-100px"}}>
+        <Skills />
+      </div>
+
+      <Work />
+      <Callout />
+      <Certif />
+    </>
+  );
+};
+export default Main;
