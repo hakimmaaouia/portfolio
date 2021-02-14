55d6efcb68716aef2447aa6c9d8cfa81c389f815
hakimmaaouia <hakimmaaouia@gmail.com>
2021-02-14 22:35:13 +0100

    fix cursor hover

--- src/Component/Bottom/Bottom.tsx
+++ src/Component/Bottom/Bottom.tsx
@@ -1,9 +1,17 @@
-import react from "react";
+import react, { useContext } from "react";
+import { ThemeContext } from "../../Cursor/Cursor";
 import "./Bottom.css";
 
 const Bottom = ({ onClick, children }: any) => {
+  const test = useContext(ThemeContext);
+
   return (
-    <div className="Bottom_container" onClick={onClick}>
+    <div
+      className="Bottom_container"
+      onClick={onClick}
+      onMouseEnter={()=>test(true)}
+      onMouseLeave={()=>test(false)}
+    >
       {children}
     </div>
   );
--- src/Cursor/Cursor.tsx
+++ src/Cursor/Cursor.tsx
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Cursor.css";
 
-
-export  const ThemeContext = React.createContext("defaultTheme");
+export const ThemeContext = React.createContext<any>(null);
 
 const Cursor = (props: any) => {
- 
-
+  const [theme, settheme] = useState<boolean>(false);
   const [position, setPosition] = useState<any>({ top: "0", left: "0" });
 
   return (
-    <ThemeContext.Provider value={"theme"}>
-    <div
-      onMouseMove={(e) =>
-        setPosition({ top: `${e.pageY}px`, left: `${e.pageX}px` })
-      }
-    >
-      <div className="cursor" style={position}></div>
-      {props.children}
-    </div>
+    <ThemeContext.Provider value={settheme}>
+      <div
+        onMouseMove={(e) =>
+          setPosition({ top: `${e.pageY}px`, left: `${e.pageX}px` })
+        }
+      >
+        <div
+          className={`cursor` + (theme ? " active" : " null")}
+          style={position}
+        ></div>
+        {props.children}
+      </div>
     </ThemeContext.Provider>
   );
 };
