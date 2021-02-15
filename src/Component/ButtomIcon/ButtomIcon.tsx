f377d8b13618e47aa71794798cd8d640586eb22c
hakimmaaouia <hakimmaaouia@gmail.com>
2021-02-15 22:02:29 +0100

    ui fix

--- src/Component/ButtomIcon/ButtomIcon.tsx
+++ src/Component/ButtomIcon/ButtomIcon.tsx
@@ -1,8 +1,14 @@
-import React from "react"
+import React,{useContext} from "react"
+import { ThemeContext } from "../../Cursor/Cursor";
 import "./ButtomIcon.css"
 const ButtomIcon=({logo}:any)=>{
+  const test = useContext(ThemeContext);
+
     return(
-<div className="ButtomIconContainer">
+<div className="ButtomIconContainer" 
+      onMouseEnter={()=>test(true)}
+      onMouseLeave={()=>test(false)}
+      >
   <img src={logo} className="Social_media"></img>
     </div>
     )
--- src/Component/navbar/Navbar.tsx
+++ src/Component/navbar/Navbar.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../Assets/logo.png";
 import Bottom from "../Bottom/Bottom";
+import { ThemeContext } from "../../Cursor/Cursor";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const test = useContext(ThemeContext);
   return (
     <div className="nav">
       <Link to="/" className="logo">
-        <img src={logo} ></img>
+        <img src={logo}></img>
       </Link>
       <ul>
         <li>
           <Link to="/"> Home</Link>
         </li>
 
-        <li>Projects</li>
-        <li>Articles</li>
-        <li>Contact</li>
+        <li onMouseEnter={() => test(true)} onMouseLeave={() => test(false)}>
+          Projects
+        </li>
+        <li onMouseEnter={() => test(true)} onMouseLeave={() => test(false)}>
+          Articles
+        </li>
+        <li onMouseEnter={() => test(true)} onMouseLeave={() => test(false)}>
+          Contact
+        </li>
         <li>
           <Link to="Contact">
             <Bottom>say hello</Bottom>
