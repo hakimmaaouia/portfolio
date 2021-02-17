ea349b2a78a231aa9f77820103c18b6b5227bd9e
hakimmaaouia <hakimmaaouia@gmail.com>
2021-02-17 20:03:04 +0100

    fix works

--- src/Component/Works/WorkCard.tsx
+++ src/Component/Works/WorkCard.tsx
@@ -1,16 +1,22 @@
-import React from "react"
-import "./WorkCard.css"
-
-const WorkCard=()=>{
-
-    return(
-        <div className="container">
-        <img src="https://unsplash.it/400/400" alt=""></img>
-        <div className="overlay">
-          <span>Hello.</span>
-        </div>
+import React from "react";
+import "./WorkCard.css";
+import Bottom from "../Bottom/Bottom";
+const WorkCard = () => {
+  return (
+    <div className="container">
+      <img src="https://unsplash.it/400/400" alt=""></img>
+      <div className="overlay">
+        <span>
+          <p>
+          High-end, custom residential renovaters serving Fraser Valley homeowners.
+          </p>
+          <a href="https://www.google.com/"  target="_blank">
+          <Bottom>Visit Website</Bottom>
+          </a>
+        </span>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default WorkCard;
